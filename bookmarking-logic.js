ec940498d449b701ac746f785ec9007056c3a9df
Chandler Simon <108281608+chandlersimon@users.noreply.github.com>
2023-01-12 23:04:15 -0800

    Create bookmarking-logic.js

---
+++ bookmarking-logic.js
@@ -0,0 +1,287 @@
+// Bookmark button updates and sets local storage
+$('.bookmark.a').click(function() {setTimeout(() => {var a = $('#bookmark-value').text();localStorage.setItem('a', a);}, 1)});
+$('.bookmark.b').click(function() {setTimeout(() => {var b = $('#bookmark-value').text();localStorage.setItem('b', b);}, 1)});
+$('.bookmark.c').click(function() {setTimeout(() => {var c = $('#bookmark-value').text();localStorage.setItem('c', c);}, 1)});
+$('.bookmark.d').click(function() {setTimeout(() => {var d = $('#bookmark-value').text();localStorage.setItem('d', d);}, 1)});
+$('.bookmark.e').click(function() {setTimeout(() => {var e = $('#bookmark-value').text();localStorage.setItem('e', e);}, 1)});
+$('.bookmark.f').click(function() {setTimeout(() => {var f = $('#bookmark-value').text();localStorage.setItem('f', f);}, 1)});
+$('.bookmark.g').click(function() {setTimeout(() => {var g = $('#bookmark-value').text();localStorage.setItem('g', g);}, 1)});
+$('.bookmark.h').click(function() {setTimeout(() => {var h = $('#bookmark-value').text();localStorage.setItem('h', h);}, 1)});
+$('.bookmark.i').click(function() {setTimeout(() => {var i = $('#bookmark-value').text();localStorage.setItem('i', i);}, 1)});
+$('.bookmark.j').click(function() {setTimeout(() => {var j = $('#bookmark-value').text();localStorage.setItem('j', j);}, 1)});
+$('.bookmark.k').click(function() {setTimeout(() => {var k = $('#bookmark-value').text();localStorage.setItem('k', k);}, 1)});
+$('.bookmark.l').click(function() {setTimeout(() => {var l = $('#bookmark-value').text();localStorage.setItem('l', l);}, 1)});
+$('.bookmark.m').click(function() {setTimeout(() => {var m = $('#bookmark-value').text();localStorage.setItem('m', m);}, 1)});
+$('.bookmark.n').click(function() {setTimeout(() => {var n = $('#bookmark-value').text();localStorage.setItem('n', n);}, 1)});
+$('.bookmark.o').click(function() {setTimeout(() => {var o = $('#bookmark-value').text();localStorage.setItem('o', o);}, 1)});
+$('.bookmark.p').click(function() {setTimeout(() => {var p = $('#bookmark-value').text();localStorage.setItem('p', p);}, 1)});
+$('.bookmark.q').click(function() {setTimeout(() => {var q = $('#bookmark-value').text();localStorage.setItem('q', q);}, 1)});
+$('.bookmark.r').click(function() {setTimeout(() => {var r = $('#bookmark-value').text();localStorage.setItem('r', r);}, 1)});
+$('.bookmark.s').click(function() {setTimeout(() => {var s = $('#bookmark-value').text();localStorage.setItem('s', s);}, 1)});
+
+// Bookmark button updates and sets local storage
+$('.resetsthis.a').click(function() {setTimeout(() => {var a = $('#bookmark-value').text();localStorage.setItem('a', a);}, 1)});
+$('.resetsthis.b').click(function() {setTimeout(() => {var b = $('#bookmark-value').text();localStorage.setItem('b', b);}, 1)});
+$('.resetsthis.c').click(function() {setTimeout(() => {var c = $('#bookmark-value').text();localStorage.setItem('c', c);}, 1)});
+$('.resetsthis.d').click(function() {setTimeout(() => {var d = $('#bookmark-value').text();localStorage.setItem('d', d);}, 1)});
+$('.resetsthis.e').click(function() {setTimeout(() => {var e = $('#bookmark-value').text();localStorage.setItem('e', e);}, 1)});
+$('.resetsthis.f').click(function() {setTimeout(() => {var f = $('#bookmark-value').text();localStorage.setItem('f', f);}, 1)});
+$('.resetsthis.g').click(function() {setTimeout(() => {var g = $('#bookmark-value').text();localStorage.setItem('g', g);}, 1)});
+$('.resetsthis.h').click(function() {setTimeout(() => {var h = $('#bookmark-value').text();localStorage.setItem('h', h);}, 1)});
+$('.resetsthis.i').click(function() {setTimeout(() => {var i = $('#bookmark-value').text();localStorage.setItem('i', i);}, 1)});
+$('.resetsthis.j').click(function() {setTimeout(() => {var j = $('#bookmark-value').text();localStorage.setItem('j', j);}, 1)});
+$('.resetsthis.k').click(function() {setTimeout(() => {var k = $('#bookmark-value').text();localStorage.setItem('k', k);}, 1)});
+$('.resetsthis.l').click(function() {setTimeout(() => {var l = $('#bookmark-value').text();localStorage.setItem('l', l);}, 1)});
+$('.resetsthis.m').click(function() {setTimeout(() => {var m = $('#bookmark-value').text();localStorage.setItem('m', m);}, 1)});
+$('.resetsthis.n').click(function() {setTimeout(() => {var n = $('#bookmark-value').text();localStorage.setItem('n', n);}, 1)});
+$('.resetsthis.o').click(function() {setTimeout(() => {var o = $('#bookmark-value').text();localStorage.setItem('o', o);}, 1)});
+$('.resetsthis.p').click(function() {setTimeout(() => {var p = $('#bookmark-value').text();localStorage.setItem('p', p);}, 1)});
+$('.resetsthis.q').click(function() {setTimeout(() => {var q = $('#bookmark-value').text();localStorage.setItem('q', q);}, 1)});
+$('.resetsthis.r').click(function() {setTimeout(() => {var r = $('#bookmark-value').text();localStorage.setItem('r', r);}, 1)});
+$('.resetsthis.s').click(function() {setTimeout(() => {var s = $('#bookmark-value').text();localStorage.setItem('s', s);}, 1)});
+
+
+// Reset button updates and sets all local storage to empty
+$('.bookmark.reset').click(function() {
+setTimeout(() => {
+var resets = $('#bookmark-value').text();
+localStorage.setItem('a', resets);
+$(".resetsthis.a").removeClass("visible");
+localStorage.setItem('b', resets);
+$(".resetsthis.b").removeClass("visible");
+localStorage.setItem('c', resets);
+$(".resetsthis.c").removeClass("visible");
+localStorage.setItem('d', resets);
+$(".resetsthis.d").removeClass("visible");
+localStorage.setItem('e', resets);
+$(".resetsthis.e").removeClass("visible");
+localStorage.setItem('f', resets);
+$(".resetsthis.f").removeClass("visible");
+localStorage.setItem('g', resets);
+$(".resetsthis.g").removeClass("visible");
+localStorage.setItem('h', resets);
+$(".resetsthis.h").removeClass("visible");
+localStorage.setItem('i', resets);
+$(".resetsthis.i").removeClass("visible");
+localStorage.setItem('j', resets);
+$(".resetsthis.j").removeClass("visible");
+localStorage.setItem('k', resets);
+$(".resetsthis.k").removeClass("visible");
+localStorage.setItem('l', resets);
+$(".resetsthis.l").removeClass("visible");
+localStorage.setItem('m', resets);
+$(".resetsthis.m").removeClass("visible");
+localStorage.setItem('n', resets);
+$(".resetsthis.n").removeClass("visible");
+localStorage.setItem('o', resets);
+$(".resetsthis.o").removeClass("visible");
+localStorage.setItem('p', resets);
+$(".resetsthis.p").removeClass("visible");
+localStorage.setItem('q', resets);
+$(".resetsthis.q").removeClass("visible");
+localStorage.setItem('r', resets);
+$(".resetsthis.r").removeClass("visible");
+localStorage.setItem('s', resets);
+$(".resetsthis.s").removeClass("visible");
+}, 1)
+});  
+
+// Logic to change the bookmark icon from filled in or outlined 
+if (localStorage.getItem('a')==='?a'){$(".resetsthis.a").addClass("visible");}else{$(".resetsthis.a").removeClass("visibile");}
+if (localStorage.getItem('b')==='?b'){$(".resetsthis.b").addClass("visible");}else{$(".resetsthis.b").removeClass("visibile");}
+if (localStorage.getItem('c')==='?c'){$(".resetsthis.c").addClass("visible");}else{$(".resetsthis.c").removeClass("visibile");}
+if (localStorage.getItem('d')==='?d'){$(".resetsthis.d").addClass("visible");}else{$(".resetsthis.d").removeClass("visibile");}
+if (localStorage.getItem('e')==='?e'){$(".resetsthis.e").addClass("visible");}else{$(".resetsthis.e").removeClass("visibile");}
+if (localStorage.getItem('f')==='?f'){$(".resetsthis.f").addClass("visible");}else{$(".resetsthis.f").removeClass("visibile");}
+if (localStorage.getItem('g')==='?g'){$(".resetsthis.g").addClass("visible");}else{$(".resetsthis.g").removeClass("visibile");}
+if (localStorage.getItem('h')==='?h'){$(".resetsthis.h").addClass("visible");}else{$(".resetsthis.h").removeClass("visibile");}
+if (localStorage.getItem('i')==='?i'){$(".resetsthis.i").addClass("visible");}else{$(".resetsthis.i").removeClass("visibile");}
+if (localStorage.getItem('j')==='?j'){$(".resetsthis.j").addClass("visible");}else{$(".resetsthis.j").removeClass("visibile");}
+if (localStorage.getItem('k')==='?k'){$(".resetsthis.k").addClass("visible");}else{$(".resetsthis.k").removeClass("visibile");}
+if (localStorage.getItem('l')==='?l'){$(".resetsthis.l").addClass("visible");}else{$(".resetsthis.l").removeClass("visibile");}
+if (localStorage.getItem('m')==='?m'){$(".resetsthis.m").addClass("visible");}else{$(".resetsthis.m").removeClass("visibile");}
+if (localStorage.getItem('n')==='?n'){$(".resetsthis.n").addClass("visible");}else{$(".resetsthis.n").removeClass("visibile");}
+if (localStorage.getItem('o')==='?o'){$(".resetsthis.o").addClass("visible");}else{$(".resetsthis.o").removeClass("visibile");}
+if (localStorage.getItem('p')==='?p'){$(".resetsthis.p").addClass("visible");}else{$(".resetsthis.p").removeClass("visibile");}
+if (localStorage.getItem('q')==='?q'){$(".resetsthis.q").addClass("visible");}else{$(".resetsthis.q").removeClass("visibile");}
+if (localStorage.getItem('r')==='?r'){$(".resetsthis.r").addClass("visible");}else{$(".resetsthis.r").removeClass("visibile");}
+if (localStorage.getItem('s')==='?s'){$(".resetsthis.s").addClass("visible");}else{$(".resetsthis.s").removeClass("visibile");}
+
+  
+// Logic to change the bookmark icon from filled in or outlined 
+$('.bookmark.a').click(function() {setTimeout(() => {if (localStorage.getItem('a') === '?a'){
+  $(".resetsthis.a").addClass("visible");}else {$(".resetsthis.a.visibile").removeClass("visible");}},1)});
+$('.bookmark.b').click(function() {setTimeout(() => {if (localStorage.getItem('b') === '?b'){
+  $(".resetsthis.b").addClass("visible");}else {$(".resetsthis.b.visibile").removeClass("visible");}},1)});
+$('.bookmark.c').click(function() {setTimeout(() => {if (localStorage.getItem('c') === '?c'){
+  $(".resetsthis.c").addClass("visible");}else {$(".resetsthis.c.visibile").removeClass("visible");}},1)});
+$('.bookmark.d').click(function() {setTimeout(() => {if (localStorage.getItem('d') === '?d'){
+  $(".resetsthis.d").addClass("visible");}else {$(".resetsthis.d.visibile").removeClass("visible");}},1)});  
+$('.bookmark.e').click(function() {setTimeout(() => {if (localStorage.getItem('e') === '?e'){
+  $(".resetsthis.e").addClass("visible");}else {$(".resetsthis.e.visibile").removeClass("visible");}},1)});  
+$('.bookmark.f').click(function() {setTimeout(() => {if (localStorage.getItem('f') === '?f'){
+  $(".resetsthis.f").addClass("visible");}else {$(".resetsthis.f.visibile").removeClass("visible");}},1)});  
+$('.bookmark.g').click(function() {setTimeout(() => {if (localStorage.getItem('g') === '?g'){
+  $(".resetsthis.g").addClass("visible");}else {$(".resetsthis.g.visibile").removeClass("visible");}},1)});  
+$('.bookmark.h').click(function() {setTimeout(() => {if (localStorage.getItem('h') === '?h'){
+  $(".resetsthis.h").addClass("visible");}else {$(".resetsthis.h.visibile").removeClass("visible");}},1)});  
+$('.bookmark.i').click(function() {setTimeout(() => {if (localStorage.getItem('i') === '?i'){
+  $(".resetsthis.i").addClass("visible");}else {$(".resetsthis.i.visibile").removeClass("visible");}},1)});  
+$('.bookmark.j').click(function() {setTimeout(() => {if (localStorage.getItem('j') === '?j'){
+  $(".resetsthis.j").addClass("visible");}else {$(".resetsthis.j.visibile").removeClass("visible");}},1)});  
+$('.bookmark.k').click(function() {setTimeout(() => {if (localStorage.getItem('k') === '?k'){
+  $(".resetsthis.k").addClass("visible");}else {$(".resetsthis.k.visibile").removeClass("visible");}},1)});  
+$('.bookmark.l').click(function() {setTimeout(() => {if (localStorage.getItem('l') === '?l'){
+  $(".resetsthis.l").addClass("visible");}else {$(".resetsthis.l.visibile").removeClass("visible");}},1)});  
+$('.bookmark.m').click(function() {setTimeout(() => {if (localStorage.getItem('m') === '?m'){
+  $(".resetsthis.m").addClass("visible");}else {$(".resetsthis.m.visibile").removeClass("visible");}},1)});  
+$('.bookmark.n').click(function() {setTimeout(() => {if (localStorage.getItem('n') === '?n'){
+  $(".resetsthis.n").addClass("visible");}else {$(".resetsthis.n.visibile").removeClass("visible");}},1)});  
+$('.bookmark.o').click(function() {setTimeout(() => {if (localStorage.getItem('o') === '?o'){
+  $(".resetsthis.o").addClass("visible");}else {$(".resetsthis.o.visibile").removeClass("visible");}},1)});  
+$('.bookmark.p').click(function() {setTimeout(() => {if (localStorage.getItem('p') === '?p'){
+  $(".resetsthis.p").addClass("visible");}else {$(".resetsthis.p.visibile").removeClass("visible");}},1)});  
+$('.bookmark.q').click(function() {setTimeout(() => {if (localStorage.getItem('q') === '?q'){
+  $(".resetsthis.q").addClass("visible");}else {$(".resetsthis.q.visibile").removeClass("visible");}},1)});  
+$('.bookmark.r').click(function() {setTimeout(() => {if (localStorage.getItem('r') === '?r'){
+  $(".resetsthis.r").addClass("visible");}else {$(".resetsthis.r.visibile").removeClass("visible");}},1)});  
+$('.bookmark.s').click(function() {setTimeout(() => {if (localStorage.getItem('s') === '?s'){
+  $(".resetsthis.s").addClass("visible");}else {$(".resetsthis.s.visibile").removeClass("visible");}},1)});  
+
+// Setting the values for local storage keys 
+$('.bookmark.a').click(function() {
+  if (localStorage.getItem('a') === '?a'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.a").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?a";
+}});
+$('.bookmark.b').click(function() {
+  if (localStorage.getItem('b') === '?b'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.b").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?b";
+}});
+$('.bookmark.c').click(function() {
+  if (localStorage.getItem('c') === '?c'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.c").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?c";
+}});
+$('.bookmark.d').click(function() {
+  if (localStorage.getItem('d') === '?d'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.d").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?d";
+}});
+$('.bookmark.e').click(function() {
+  if (localStorage.getItem('e') === '?e'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.e").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?e";
+}});
+$('.bookmark.f').click(function() {
+  if (localStorage.getItem('f') === '?f'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.f").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?f";
+}});
+$('.bookmark.g').click(function() {
+  if (localStorage.getItem('g') === '?g'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.g").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?g";
+}});
+$('.bookmark.h').click(function() {
+  if (localStorage.getItem('h') === '?h'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.h").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?h";
+}});
+$('.bookmark.i').click(function() {
+  if (localStorage.getItem('i') === '?i'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.i").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?i";
+}});
+$('.bookmark.j').click(function() {
+  if (localStorage.getItem('j') === '?j'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.j").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?j";
+}});
+$('.bookmark.k').click(function() {
+  if (localStorage.getItem('k') === '?k'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.k").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?k";
+}});
+$('.bookmark.l').click(function() {
+  if (localStorage.getItem('l') === '?l'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.l").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?l";
+}});
+$('.bookmark.m').click(function() {
+  if (localStorage.getItem('m') === '?m'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.m").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?m";
+}});
+$('.bookmark.n').click(function() {
+  if (localStorage.getItem('n') === '?n'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.n").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?n";
+}});
+$('.bookmark.o').click(function() {
+  if (localStorage.getItem('o') === '?o'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.o").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?o";
+}});
+$('.bookmark.p').click(function() {
+  if (localStorage.getItem('p') === '?p'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.p").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?p";
+}});
+$('.bookmark.q').click(function() {
+  if (localStorage.getItem('q') === '?q'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.q").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?q";
+}});
+$('.bookmark.r').click(function() {
+  if (localStorage.getItem('r') === '?r'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.r").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?r";
+}});
+$('.bookmark.s').click(function() {
+  if (localStorage.getItem('s') === '?s'){
+    document.getElementById("bookmark-value").innerHTML = "?0";
+    $(".resetsthis.s").removeClass("visible");}
+  else {
+    document.getElementById("bookmark-value").innerHTML = "?s";
+}});
+$('.reset').click(function() {
+  document.getElementById("bookmark-value").innerHTML = "?0";
+});
