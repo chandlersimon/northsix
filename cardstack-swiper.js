54387b63f8744504e50b4c13ba386bb5697a2f10
Chandler Simon <108281608+chandlersimon@users.noreply.github.com>
2023-01-13 07:33:59 -0800

    Create cardstack-swiper.js

---
+++ cardstack-swiper.js
@@ -0,0 +1,14 @@
+effect: "cards",
+cardsEffect: {
+perSlideOffset: 5,
+perSlideRotate: 2,
+slideShadows: false,},
+grabCursor: true,
+loop: true,
+keyboard: true,
+threshold: 5,
+navigation: {
+	nextEl: '.right-arrow',
+	prevEl: '.left-arrow',
+},
+});
